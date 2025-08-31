2c7c29167d4d9b87184e93cb7426ea2b07ad3490
Luis Almazan <luis.almaza.garcia@gmail.com>
2025-08-31 14:49:38 -0600

    Prueba ejecucion

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Este script actualiza el repositorio, reconstruye y reinicia los contenedores de Docker.
+
+# Navegar al directorio del proyecto
+cd /opt/repositorio/bahias-front
+# Actualizar el repositorio
+git pull origin main
+# Construir y reiniciar los contenedores de Docker
+docker compose build
+docker compose down 
+docker compose up -d
+
+# Limpiar recursos no utilizados de Docker
+docker builder prune -f 
+docker image prune -f
+
+# Enviar una notificación de que el despliegue ha finalizado
+echo "Despliegue completado"
