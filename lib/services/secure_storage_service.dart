d7a28c553291aea33ca16b8346461eb6f7b10412
Luis Almazan <luis.almaza.garcia@gmail.com>
2025-08-31 14:15:41 -0600

    cambios  login

--- lib/services/secure_storage_service.dart
+++ lib/services/secure_storage_service.dart
@@ -1,10 +1,37 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SecureStorageService {
   static const _tokenKey = 'auth_token';
-  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
-  Future<void> saveToken(String token) => _storage.write(key: _tokenKey, value: token);
-  Future<String?> getToken() => _storage.read(key: _tokenKey);
-  Future<void> deleteToken() => _storage.delete(key: _tokenKey);
+  final FlutterSecureStorage? _secure =
+      kIsWeb ? null : const FlutterSecureStorage();
+
+  Future<void> saveToken(String token) async {
+    if (kIsWeb) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString(_tokenKey, token);
+    } else {
+      await _secure!.write(key: _tokenKey, value: token);
+    }
+  }
+
+  Future<String?> getToken() async {
+    if (kIsWeb) {
+      final prefs = await SharedPreferences.getInstance();
+      return prefs.getString(_tokenKey);
+    } else {
+      return _secure!.read(key: _tokenKey);
+    }
+  }
+
+  Future<void> deleteToken() async {
+    if (kIsWeb) {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove(_tokenKey);
+    } else {
+      await _secure!.delete(key: _tokenKey);
+    }
+  }
 }
