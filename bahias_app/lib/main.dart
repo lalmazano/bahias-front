d99bfa77ad1e64c4a53a7dbf0ca5b58bd44dd74d
Luis Almazan <luis.almaza.garcia@gmail.com>
2025-10-29 00:26:54 -0600

    Conexion con firebase finalizada

--- bahias_app/lib/main.dart
+++ bahias_app/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 import 'screens/dashboard_screen.dart';
 import 'services/auth_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
